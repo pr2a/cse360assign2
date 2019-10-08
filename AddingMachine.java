e00d1d44e253c4e2597630d4b0cfa197218681c9
pr2a <31780665+pr2a@users.noreply.github.com>
2019-10-08 14:24:08 -0700

    Initial Version

---
+++ AddingMachine.java
@@ -0,0 +1,62 @@
+/* Author: Priya Ganguly
+ * Class ID: 85141
+ * Assignment: 2
+ * Description: This program performs the add and subtract mathematical functions using integer numbers.
+ */
+
+package cse360assign2;
+
+/** Adding Machine is a class that starts with 0 and has some integers added and subtracted to
+ * it; as the history is recorded, it is then printed at the end of the machine.
+ * 
+ * @author priyaganguly
+ * @version 1.0
+ */
+public class AddingMachine {
+
+	/** This is a global variable: {@link #total} **/
+	private int total;
+	
+	/** Class constructor initializes the total to 0
+	 * @return args Unused.
+	 */
+	public AddingMachine() {
+		total = 0; // not needed - included for clarity
+	}
+	
+	/** returns the current total 
+	 * @param args Unused.
+	 * @return int This returns the total answer after the operations are applied.
+	 */
+	public int getTotal() {
+		return 0;
+	}
+	
+	/** This method is used to add an integer to the total. 
+	 * @param value This is the parameter for the add method.
+	 */
+	public void add(int value) {
+
+	}
+	
+	/** This method is used to subtract an integer from the total. 
+	 * @param value This is the parameter for the subtract method.
+	 */
+	public void subtract(int value) {
+
+	}
+	
+	/** This method returns the entire operation done
+	 * @param args Unused.
+	 * @return string This returns a string
+	 */
+	public String toString() {
+		return "";
+	}
+	
+	/** This method resets the calculation history.
+	 * @param args Unused.
+	 */
+	public void clear() {
+	}
+}
